d13712bb9db3af8c3a20afb949c8d42451f8d572
zzyzzyzzy123 <782755046@qq.com>
2019-08-09 13:08:56 +0800

    microservicecloud-config配置

---
+++ microservicecloud-config-3344/src/test/java/top/zzy/microservicecloudconfig3344/MicroservicecloudConfig3344ApplicationTests.java
@@ -0,0 +1,16 @@
+package top.zzy.microservicecloudconfig3344;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroservicecloudConfig3344ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
